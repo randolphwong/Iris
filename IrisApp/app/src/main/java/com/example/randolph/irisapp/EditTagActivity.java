0517e0afd8e8a499bfd95635cb582acebfcb409c
LiuSu <liusumy@gmail.com>
2015-12-04 12:05:23 +0800

    add functions without database

---
+++ IrisApp/app/src/main/java/com/example/randolph/irisapp/EditTagActivity.java
@@ -0,0 +1,75 @@
+package com.example.randolph.irisapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class EditTagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_tag);
+        TextView tagName = (TextView)findViewById(R.id.tagname);
+        tagName.setText("Editing Tag: " + MainActivity.tagName);
+        final ToggleButton toggle = (ToggleButton)findViewById(R.id.enable);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            /**
+             * TODO: Implement onCheckedChanged method. When the toggle button change status, change the status enabled/disabled of this tag
+             *
+             */
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) toggle.setText("Enabled");
+                else toggle.setText("Disabled");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_tag, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * TODO: Implement delete function. When the delete method is called, delete that tag from database
+     *
+     */
+
+    public void delete(View view){
+        Toast.makeText(getApplicationContext(),MainActivity.tagName + " Deleted",Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * TODO: Implement report lost function. When the reportLost method is called, mark the corresponding tag in database as lost
+     * @param view
+     */
+    public void reportLost(View view){
+        Toast.makeText(getApplicationContext(),MainActivity.tagName + " Reported",Toast.LENGTH_SHORT).show();
+    }
+
+
+}
--- IrisApp/app/src/main/java/com/example/randolph/irisapp/MainActivity.java
+++ IrisApp/app/src/main/java/com/example/randolph/irisapp/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.randolph.irisapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView tagList;
+    private ArrayAdapter arrayAdapter;
+    public static String tagName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
     }
 
     @Override
@@ -33,5 +42,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * TODO: Modify the method so it will get data from SQL database instead of the tempdatabase
+     * need to return a string array to method arrayAdapter()
+     */
+    public void initialize(){
+        final TempDatabase database = new TempDatabase();
+        String[] data = database.getData();
+        tagList = (ListView)findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,data);
+        tagList.setAdapter(arrayAdapter);
+
+        tagList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tagName = database.getNameByIndex(position);
+                Intent intent = new Intent(MainActivity.this,EditTagActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
